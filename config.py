4abcbae1142eb053693cad15fd0bc5c37dd0822f
Gaikwad Kunal Ganpat <162322840+KunalG932@users.noreply.github.com>
2024-05-26 14:21:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 {
-    "api_id": xxxxx, // Put your API ID here
-    "api_hash": "xxxxx", // Put your API Hash here
-    "admin": xxxxx, // Put your Admin Telegram user ID here
+    "api_id": 22418774, // Put your API ID here
+    "api_hash": "d8c8dab274f9a811814a6a96d044028e", // Put your API Hash here
+    "admin": 6239759268, // Put your Admin Telegram user ID here
     "auto_upgrade": true, // Set to true to enable auto-upgrade
-    "max_charge_level": 1, // Set maximum charge level
-    "max_energy_level": 1, // Set maximum energy level
-    "max_tap_level": 1 // Set maximum tap level
+    "max_charge_level": 100, // Set maximum charge level
+    "max_energy_level": 100, // Set maximum energy level
+    "max_tap_level": 100 // Set maximum tap level
 }
